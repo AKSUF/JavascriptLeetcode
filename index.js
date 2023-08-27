71e4baa277b9b0989ee0bce4ae13c219745d9e93
AKSUF <abu053125@gmail.com>
2023-08-27 21:49:30 +0600

    findthirdmaximumnumber

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const arr = [1, 2, 3, 4, 5, 1]
+const arr = [1, 2, 3, 4, 5];
 console.log(arr);
-const duplicate = findDuplicate(arr);
+const maximum = findthirdMaxinumber(arr);
+console.log(maximum);
 
-console.log(duplicate);
+function findthirdMaxinumber(arr) {
+    let max1 = arr[0];
+    let max2 = arr[0];
+    let max3 = arr[0];
 
-function findDuplicate(arr) {
-    const set = new Set();
-    for (let i = 0; arr.length; i++) {
-        if (set.has(arr[i])) {
-
-            return arr[i];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max1) {
+            max3 = max2;
+            max2 = max1;
+            max1 = arr[i]
         } else {
-            set.add(arr[i])
+            max3 = max2;
+            max2 = arr[i];
         }
     }
+    return max3;
+
 }
