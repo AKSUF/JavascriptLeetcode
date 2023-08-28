ebe5b027cf44d406bc51b13b15f46f7168029eb5
AKSUF <abu053125@gmail.com>
2023-08-28 18:58:39 +0600

    checkduplicants

--- index.js
+++ index.js
@@ -1,40 +1,19 @@
-function fourSum(nums, target) {
-    const result = [];
-    nums.sort((a, b) => a - b);
+const nums = [1, 1, 2];
+const uniqueCount = removeDuplicants(nums);
+console.log(uniqueCount);
+console.log(nums.slice(0, uniqueCount));
 
-    for (let i = 0; i < nums.length - 3; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) {
-            continue;
-        }
+function removeDuplicants(nums) {
+    if (nums.length === 0) {
+        return 0;
+    }
+    let k = 1;
 
-        for (let j = i + 1; j < nums.length - 2; j++) {
-            if (j > i + 1 && nums[j] === nums[j - 1]) {
-                continue;
-            }
-
-            for (let k = j + 1; k < nums.length - 1; k++) {
-                if (k > j + 1 && nums[k] === nums[k - 1]) {
-                    continue;
-                }
-
-                for (let l = k + 1; l < nums.length; l++) {
-                    if (l > k + 1 && nums[l] === nums[l - 1]) {
-                        continue;
-                    }
-
-                    const sum = nums[i] + nums[j] + nums[k] + nums[l];
-                    if (sum === target) {
-                        result.push([nums[i], nums[j], nums[k], nums[l]]);
-                    }
-                }
-            }
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] != nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
         }
     }
-
-    return result;
+    return k;
 }
-
-const nums = [1, 0, -1, 0, -2, 2];
-const target = 0;
-const result = fourSum(nums, target);
-console.log(result); // Outputs: [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
