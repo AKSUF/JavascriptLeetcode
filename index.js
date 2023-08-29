bfb57b20da48c91748e4a45202f9e82491579765
AKSUF <abu053125@gmail.com>
2023-08-29 16:00:39 +0600

    insertposition

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-let target = 8;
-
-
-let result = searchRange(nums, target);
-
+const nums = [1, 2, 3, 4, 5, 6, 7]
+let target = 5;
+let result = insertposition(nums, target);
 console.log(result);
 
-function searchRange(nums, target) {
-    let firstOcurrencr = findFirstOccurnce(nums, target);
+function insertposition(nums, target) {
+    let i;
 
-    if (firstOcurrencr === -1) {
-        return [-1, -1];
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] >= target)
+            break;
     }
-    const lastOccuurence = findLastOccurence(nums, target);
-    return [firstOcurrencr, lastOccuurence];
-}
-
-function findFirstOccurnce(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === target) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-function findLastOccurence(nums, target) {
-    for (let i = nums.length - 1; i >= 0; i--) {
-        if (nums[i] === target) {
-            return i;
-        }
-    }
-    return -1;
+    return i;
 }
