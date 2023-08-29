b5cfff28008d45f670b46abb0c99192f2a2c8cb1
AKSUF <abu053125@gmail.com>
2023-08-29 09:55:29 +0600

    Rotated

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-function nextPermutation(nums) {
-    let i = nums.length - 1;
-    while (i > 0 && nums[i - 1] >= nums[i]) {
-        i--;
-    }
-    if (i == 0) {
-        nums.reverse();
-        return;
-    }
-    let j = nums.length - 1;
-    while (nums[j] <= nums[i - 1]) {
-        j--;
-    }
-    let temp = nums[i - 1];
-    nums[i - 1] = nums[j];
-    nums[j] = temp;
+function search(nums, target) {
+    const n = nums.length;
 
-    let start = i;
-    let end = nums.length - 1;
-    while (start < end) {
-        temp = nums[start];
-        nums[start] = nums[end];
-        start++;
-        end--;
+    let left = 0;
+    let right = n - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target <= nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if (nums[mid] <= target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
     }
 
+    return -1;
 }
 
-
-nums = [1, 2, 3];
-
-nextPermutation(nums);
-
-console.log(nums);
+const nums = [4, 5, 6, 7, 0, 1, 2];
+const target = 0;
+const result = search(nums, target);
+console.log(result); // Output: 4
